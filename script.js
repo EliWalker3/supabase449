915d9be258624f7a55fe3f4455b6b8ca0ed22e6d
Elijah Walker <walke861@msu.edu>
2025-03-13 16:47:47 -0400

    current state of file - table wont load

---
+++ script.js
@@ -0,0 +1,17 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
+const supabaseUrl = 'https://zbdfnquswzpzsbiidtmt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpiZGZucXVzd3pwenNiaWlkdG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzM4MTQsImV4cCI6MjA1NzQ0OTgxNH0.ipt_BjLAeATloDQiGy7ArFM0-wTj9HsBoHHNBq-9RMo'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+  for (let book of books) {
+      let bookList = document.getElementById("books");
+      bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td></tr>`;
+  }  
+}
+
+getBooks()
