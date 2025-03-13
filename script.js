b39cab0042568ab8dbf7d7f0f8ce45824ce309b2
Elijah Walker <walke861@msu.edu>
2025-03-13 17:51:18 -0400

    updated supabase to fix site errors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js"
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.7.1"
 const supabaseUrl = 'https://zbdfnquswzpzsbiidtmt.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpiZGZucXVzd3pwenNiaWlkdG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzM4MTQsImV4cCI6MjA1NzQ0OTgxNH0.ipt_BjLAeATloDQiGy7ArFM0-wTj9HsBoHHNBq-9RMo'
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -10,7 +10,7 @@ async function getBooks() {
 
   for (let book of books) {
       let bookList = document.getElementById("books");
-      bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td></tr>`;
+      bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.title}</td></tr>`;
   }  
 }
 
